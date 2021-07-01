f65c1536d880997d35d9fd9b18f607e8b1f17611
Strong Uzumaki <suzumaki@il-g5.msk.21-school.ru>
2021-07-01 20:55:17 +0300

    02 done

---
+++ CPP_01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	std::string 	str = "HI THIS IS BRAIN";
+	std::string&	stringREF = str;
+	std::string*	stringPTR = &str;
+
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
+}
+
