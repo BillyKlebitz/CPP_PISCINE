01e46dfc49201a215ef9f98d7c539f85eee752f9
Strong Uzumaki <yara215@yandex.ru>
2021-07-04 20:54:36 +0300

    01 not done

---
+++ CPP_02/ex02/Fixed.cpp
@@ -0,0 +1,158 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(){
+	std::cout << "Default constructor called" << std::endl;
+	this->raw_bits = 0;
+}
+
+Fixed::Fixed(Fixed const & src){
+
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::Fixed(int const value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->raw_bits = value << this->fractional_bits;
+}
+
+Fixed::Fixed(float const value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	
+	int i = -1;
+	int tmp = 1;
+	while (++i < this->fractional_bits)
+		tmp *= 2;
+	this->raw_bits = roundf(value * tmp);
+}
+
+int Fixed::getRawBits( void ) const{
+
+	return (this->raw_bits);
+}
+
+void	Fixed::setRawBits( int const raw ){
+
+	this->raw_bits = raw;
+}
+
+Fixed	& Fixed::operator=(Fixed const &rhs)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->raw_bits = rhs.getRawBits();
+	return *this;
+}
+
+
+Fixed	& Fixed::operator+(Fixed const &rhs)
+{
+	this->raw_bits += rhs.getRawBits();
+	return *this;
+}
+
+Fixed	& Fixed::operator-(Fixed const &rhs)
+{
+	this->raw_bits -= rhs.getRawBits();
+	return *this;
+}
+
+Fixed	& Fixed::operator*(Fixed const &rhs)
+{
+	this->raw_bits *= rhs.getRawBits();
+	return *this;
+}
+
+Fixed	& Fixed::operator/(Fixed const &rhs)
+{
+	
+	if (rhs.getRawBits() == 0)
+	{
+		std::cout << "zero exception" << std::endl;
+		return *this;
+	}
+	this->raw_bits /= rhs.getRawBits();
+	return *this;
+}
+
+bool 	Fixed::operator>(Fixed const &rhs){
+
+	return (this->getRawBits() > rhs.getRawBits());
+}
+
+bool 	Fixed::operator<(Fixed const &rhs){
+
+	return (this->getRawBits() < rhs.getRawBits());
+}
+
+bool 	Fixed::operator>=(Fixed const &rhs){
+
+	return (this->getRawBits() >= rhs.getRawBits());
+}
+
+bool 	Fixed::operator<=(Fixed const &rhs){
+
+	return (this->getRawBits() <= rhs.getRawBits());
+}
+
+bool 	Fixed::operator==(Fixed const &rhs){
+
+	return (this->getRawBits() == rhs.getRawBits());
+}
+
+bool 	Fixed::operator!=(Fixed const &rhs){
+
+	return (this->getRawBits() == rhs.getRawBits());
+}
+
+
+Fixed &		Fixed::operator++(){
+
+	++this->raw_bits;
+	return *this;
+}
+
+Fixed &		Fixed::operator--(){
+
+	--this->raw_bits;
+	return *this;
+}
+
+Fixed	Fixed::operator++(int){
+
+	Number 
+	++this->raw_bits;
+	return *this;
+}
+
+Fixed 	Fixed::operator--(int){
+
+	--this->raw_bits;
+	return *this;
+}
+
+std::ostream	&operator<<(std::ostream & o, Fixed const &rhs)
+{
+	o << rhs.toFloat();
+	return o;
+}
+
+float Fixed::toFloat( void ) const{
+	
+	int i = -1;
+	int tmp = 1;
+	while (++i < this->fractional_bits)
+		tmp *= 2;
+
+	return (((float)raw_bits / tmp));
+}
+
+int Fixed::toInt( void ) const{
+
+	return raw_bits >> this->fractional_bits;
+}
+
+Fixed::~Fixed(){
+	std::cout << "Destructor called" << std::endl;
+}
