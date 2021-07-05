57ae5bedc6b14bbb7e316c817db2cc75926177ab
Strong Uzumaki <yara215@yandex.ru>
2021-07-05 23:22:20 +0300

    02 working, need to redo 01

--- CPP_02/ex03/main.cpp
+++ CPP_02/ex03/main.cpp
@@ -4,9 +4,11 @@
 #include <iostream>
 
 
+
 float Point::sign (Point p1, Point p2, Point p3)
 {
 	Fixed 	tmp;
+	
 	tmp = (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
 	return tmp.toFloat();
 }
@@ -20,8 +22,8 @@ bool bsp( Point const a, Point const b, Point const c, Point const point){
     d2 = Point::sign(point, b, c);
     d3 = Point::sign(point, c, a);
 
-    has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
-    has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+    has_neg = (d1 < 0.0) || (d2 < 0.0) || (d3 < 0.0);
+    has_pos = (d1 > 0.0) || (d2 > 0.0) || (d3 > 0.0);
 
     return !(has_neg && has_pos);
 
@@ -30,9 +32,9 @@ bool bsp( Point const a, Point const b, Point const c, Point const point){
 int main( void ) {
 	
 	Point	a(0.0, 0.0);
-	Point	b(20.0,20.0);
-	Point	c(0.0,20.0);
-	Point 	i(1.0,1.1);
+	Point	b(1.0,1.0);
+	Point	c(0.0,1.0);
+	Point 	i(0.5,0.6);
 
 	std::cout << bsp(a,b,c,i);
 	return 0;
